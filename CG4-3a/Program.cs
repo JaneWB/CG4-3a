d90b95aeeb7340105b9fc11b2850f00983afb6b2
JaneWB <brownjannick@att.net>
2018-08-10 23:16:43 -0500

    Add project files.

---
+++ CG4-3a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG4_3a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
